f91e0648cebb5bf8534ee5e2f105149533f56160
JOHNMOHAMED <johnmohamed0001@gmail.com>
2026-04-15 13:29:12 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
-import Navbar from "./Components/Navbar";
+import NavBar from "./Components/NavBar"
 import Home from "./Components/Home";
 import MovieList from "./Components/MovieList";
 import About from "./Components/About";
@@ -15,7 +15,7 @@ function App() {
     <BrowserRouter>
 
       {/* ✅ NAVBAR (ONLY ONCE) */}
-      <Navbar wishlistCount={wishlist.length} />
+      <NavBar wishlistCount={wishlist.length} />
 
       <Routes>
         <Route path="/" element={<Home />} />
